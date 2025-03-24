9bd1255de200acec4b60a2fee53e93299da32075
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-03-24 18:03:56 +0100

    [DONE] Ex3 DONE

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,30 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filterInitials(arr, letter){
+    let arrFiltered = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i][0] === letter){
+            arrFiltered.push(arr[i]);
+        }
+    }
+    return arrFiltered
+}
+
+const filterInitials1 = (arr, lettera) => {
+    let arrFiltered = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i][0] === lettera){
+            arrFiltered.push(arr[i]);
+        }
+    }
+    return arrFiltered
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(filterInitials(names, 'A'));
+console.log(filterInitials1(names, 'A'));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
