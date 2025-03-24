791132093341866b4e15789c597cd17c01b6c636
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-03-24 17:59:57 +0100

    [DONE] Ex2 DONE

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function initials(arr){
+    let arrinitials = [];
+    for(let i = 0; i < arr.length; i++){
+        arrinitials.push(arr[i][0]);
+    }
+    return arrinitials
+}
 
+const initials1 = (arr) => {
+    let arrinitials = [];
+    for(let i = 0; i < arr.length; i++){
+        arrinitials.push(arr[i][0]);
+    }
+    return arrinitials
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(initials(names));
+console.log(initials1(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
