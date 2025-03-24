33a7b6742980e3388a94197ce623891615eb4556
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-03-24 18:16:58 +0100

    [DONE] Ex5 Bonus  DONE

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,33 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function greet(name) {
+    const date = new Date();
+    let hour = date.getHours();
+    if(hour < 13){
+        console.log(`Buongiorno ${name}.`);
+    } else if(hour < 18){
+        console.log(`Buon pomeriggio ${name}.`);
+    } else {
+        console.log(`Buonasera ${name}.`);
+    }
+}
 
+const greet1 = (name) => {
+    const date = new Date();
+    let hour = date.getHours();
+    if(hour < 13){
+        console.log(`Buongiorno ${name}.`);
+    } else if(hour < 18){
+        console.log(`Buon pomeriggio ${name}.`);
+    } else {
+        console.log(`Buonasera ${name}.`);
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+greet(name);
+greet1(name);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
