9f70cc833ee6830a5ea56a4a99d75ff845055f9b
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-03-24 18:13:14 +0100

    [DONE] Ex04 DONE

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,9 +4,29 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function vowelCount(str){
+    const strVowels = []
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u'){
+            strVowels.push(str[i]);
+        }
+    }
+    return strVowels;
+}
 
+const vowelCount1 = (str) => {
+    const strVowels = []
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u'){
+            strVowels.push(str[i]);
+        }
+    }
+    return strVowels;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(vowelCount(word).length + ' (' + vowelCount(word) + ')');
+console.log(vowelCount1(word).length + ' (' + vowelCount1(word) + ')');
 
 
 
